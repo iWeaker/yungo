52b59a52b9b00be9440f9e62d65c05d3f705471e
iWeaker4you <iweaker4you@gmail.com>
2021-07-07 21:08:46 -0700

    Add Client and Inventory entities

--- src/Entity/Clientes.php
+++ src/Entity/Clientes.php
@@ -17,8 +17,93 @@ class Clientes
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_client;
+
+    /**
+     * @ORM\Column(type="string", length=150)
+     */
+    private $name_client;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email_client;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $phone_client;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address_client;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getIdClient(): ?int
+    {
+        return $this->id_client;
+    }
+
+    public function setIdClient(int $id_client): self
+    {
+        $this->id_client = $id_client;
+
+        return $this;
+    }
+
+    public function getNameClient(): ?string
+    {
+        return $this->name_client;
+    }
+
+    public function setNameClient(string $name_client): self
+    {
+        $this->name_client = $name_client;
+
+        return $this;
+    }
+
+    public function getEmailClient(): ?string
+    {
+        return $this->email_client;
+    }
+
+    public function setEmailClient(string $email_client): self
+    {
+        $this->email_client = $email_client;
+
+        return $this;
+    }
+
+    public function getPhoneClient(): ?string
+    {
+        return $this->phone_client;
+    }
+
+    public function setPhoneClient(string $phone_client): self
+    {
+        $this->phone_client = $phone_client;
+
+        return $this;
+    }
+
+    public function getAddressClient(): ?string
+    {
+        return $this->address_client;
+    }
+
+    public function setAddressClient(string $address_client): self
+    {
+        $this->address_client = $address_client;
+
+        return $this;
     }
 }
--- src/Entity/Inventario.php
+++ src/Entity/Inventario.php
@@ -17,8 +17,93 @@ class Inventario
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mac_inventory;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_inventory;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model_inventory;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand_inventory;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type_inventory;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getMacInventory(): ?string
+    {
+        return $this->mac_inventory;
+    }
+
+    public function setMacInventory(string $mac_inventory): self
+    {
+        $this->mac_inventory = $mac_inventory;
+
+        return $this;
+    }
+
+    public function getIdInventory(): ?int
+    {
+        return $this->id_inventory;
+    }
+
+    public function setIdInventory(int $id_inventory): self
+    {
+        $this->id_inventory = $id_inventory;
+
+        return $this;
+    }
+
+    public function getModelInventory(): ?string
+    {
+        return $this->model_inventory;
+    }
+
+    public function setModelInventory(string $model_inventory): self
+    {
+        $this->model_inventory = $model_inventory;
+
+        return $this;
+    }
+
+    public function getBrandInventory(): ?string
+    {
+        return $this->brand_inventory;
+    }
+
+    public function setBrandInventory(string $brand_inventory): self
+    {
+        $this->brand_inventory = $brand_inventory;
+
+        return $this;
+    }
+
+    public function getTypeInventory(): ?string
+    {
+        return $this->type_inventory;
+    }
+
+    public function setTypeInventory(string $type_inventory): self
+    {
+        $this->type_inventory = $type_inventory;
+
+        return $this;
     }
 }
